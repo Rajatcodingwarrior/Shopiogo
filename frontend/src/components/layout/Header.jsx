8948e432520a5bdf0c60c89b46d47a54f551fa77
Satyampandey693 <satyampandey0903@gmail.com>
2024-07-11 00:47:24 +0530

    filters and pagination added successfully

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Search from "./Search";
 
 const Header = () => {
   return (
@@ -6,27 +7,12 @@ const Header = () => {
       <div className="col-12 col-md-3 ps-5">
         <div className="navbar-brand">
           <a href="/">
-            <img src="../images/shopiogo_logo.png" alt="Shopiogo" />
+            <img src="" alt="Shopiogo" />
           </a>
         </div>
       </div>
       <div className="col-12 col-md-6 mt-2 mt-md-0">
-        <form action="your_search_action_url_here" method="get">
-          <div className="input-group">
-            <input
-              type="text"
-              id="search_field"
-              aria-describedby="search_btn"
-              className="form-control"
-              placeholder="Enter Product Name ..."
-              name="keyword"
-              value=""
-            />
-            <button id="search_btn" className="btn" type="submit">
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-          </div>
-        </form>
+        <Search />
       </div>
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
         <a href="/cart" style={{ textDecoration: "none" }}>
